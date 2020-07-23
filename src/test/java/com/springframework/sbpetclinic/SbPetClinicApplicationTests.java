27d47205c5e92c4422e67871a4064b4bdbfcb91e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2020-07-23 15:55:55 +0200

    Initial Project

---
+++ src/test/java/com/springframework/sbpetclinic/SbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
