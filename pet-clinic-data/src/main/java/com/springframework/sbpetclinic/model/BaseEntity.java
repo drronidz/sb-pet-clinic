df4bf2d979a6613a0818f920de0ada3800755c7b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2020-07-27 18:38:54 +0200

    adding base entity. Closes #10

---
+++ pet-clinic-data/src/main/java/com/springframework/sbpetclinic/model/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.springframework.sbpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) { this.id = id; }
+}
