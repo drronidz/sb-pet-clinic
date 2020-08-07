36030ab558bcf0ce1c7d746fe3e8fa25ff366753
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2020-08-07 17:21:41 +0200

    Creating PetType Map Service Impl . Closes #33

---
+++ pet-clinic-data/src/main/java/com/springframework/sbpetclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+package com.springframework.sbpetclinic.services;
+
+import com.springframework.sbpetclinic.model.PetType;
+
+/**
+ * project Name : sb-pet-clinic
+ * Created by DRRONIDZ
+ * on 07, August , 2020
+ * at 5:14 PM
+ */
+public interface PetTypeService extends CrudService<PetType,Long>
+{
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sbpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,43 @@
+package com.springframework.sbpetclinic.services.map;
+
+import com.springframework.sbpetclinic.model.PetType;
+import com.springframework.sbpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * project Name : sb-pet-clinic
+ * Created by DRRONIDZ
+ * on 07, August , 2020
+ * at 5:16 PM
+ */
+@Service
+public  class PetTypeServiceMap
+        extends AbstractMapService<PetType,Long>
+        implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
