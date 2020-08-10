d314acff38c1b4fc8bbdb827eeb453b71517dcd7
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2020-08-10 16:41:49 +0200

    Converting Vets to JPA Entities . Closes #40

--- pet-clinic-data/src/main/java/com/springframework/sbpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/sbpetclinic/model/Speciality.java
@@ -1,12 +1,19 @@
 package com.springframework.sbpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * project Name : sb-pet-clinic
  * Created by DRRONIDZ
  * on 07, August , 2020
  * at 5:02 PM
  */
+@Entity
+@Table(name="specialties")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
